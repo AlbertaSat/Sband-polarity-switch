bd73a5799176eed8a759de3b48711bc602711648
cnokes-sciencebloke <cnokes@ualberta.ca>
2022-06-23 09:51:08 -0600

    First functioning version of the arduino sketch and python script to control two relays

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
 # Sband-polarity-switch
 Polarity switch for sband groundstation arduino board
 
-The planned structure is to have a Firmata sketch running on the Arduino, and then use a Python script to command the Arduino pins on or off.
+The structure of this code is to use a custom sketch for serial communication on the arduino and then use a Python script to command the Arduino pins on or off.
 
-## Firmata Sketch
+## PythonControl Sketch
+The Arduino will have pythoncontrol.ino sketch uploaded.
+June 23 - First implementation
+Using an Arduino Mega. Pin 36 is assigned to Relay 1. Pin 38 is assigned to Relay 2. The Arduino monitors for incoming serial data.
+'L' sets Pin 36 HIGH, activating Relay 1. Relay 1 is connected to the polarity switch. The polarity switch has the RHCP port of the S-band septum polarizer on NC and the LHCP port on NO, so activating Relay 1 sets the polarity to LHCP.
+'R' sets Pin 36 LOW, deactivating Relay 1. This sets the polarity to RHCP.
+'U' sets Pin 38 HIGH, activating Relay 2. Relay 2 is connected to the TX/RX switch. The TX/RX switch has RX on NC for Downlink and TX on NO for Uplink, so activating Relay 2 sets the switch to TX for Uplink.
+'D' sets Pin 38 LOW, deactivating Relay 2. This sets the switch to RX for Downlink.
+'Q' sets both pins 36 and 38 to LOW, deactivating both relays. This sets the default configuration to RHCP and Downlink.
+
+## sband_ps_control.py
+This program uses the pyserial package for serial comms in python and the tkinter package for creating a python gui.
+
+The serial port of the Arduino is defined. Then the five commands associated with the five states defined in the PythonControl Sketch are defined. Each program prints a message to the terminal and writes a letter to the serial port of the Arduino ('L', 'R', 'U', 'D', or 'Q')
+
+The rest of the program defines the colour, size, and location of the command buttons in the gui.
+
+TO DO:
+- Improve the colours of the buttons in the GUI
+- Implement user defined inputs: ask user for the serial port, baud rate, pins of the Arduino
+- Figure out how to make this program and gui available on the ground station software website
---
+++ sband_ps_control.py
@@ -0,0 +1,73 @@
+'''
+ * Copyright (C) 2022  University of Alberta
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+'''
+'''
+ * @file sband_ps_control.py
+ * @author Charles Nokes
+ * @date 2022-06-22
+'''
+import serial
+import time
+import tkinter as tk
+
+window = tk.Tk()
+window.configure(background="gray")
+window.geometry("220x160")
+window.title("SWITCH CTRL - PYTHON GUI")
+
+megaBoard = serial.Serial('/dev/cu.usbmodem142301',9600)
+
+def switch_control():
+    print(">>> SWITCH CTRL PROGRAM <<<<\n")
+    def lhcp():
+        print("CTRL -> LHCP port -> ON")
+        megaBoard.write(b'L')
+
+    def rhcp():
+        print("CTRL -> RHCP port -> ON")
+        megaBoard.write(b'R')
+
+    def uplink():
+        print("CTRL -> UPLINK -> ON")
+        megaBoard.write(b'U')
+
+    def downlink():
+        print("CTRL -> DOWNLINK -> ON")
+        megaBoard.write(b'D')
+
+    def quit():
+        print("\n** END OF PROGRAM **")
+        megaBoard.write(b'Q')
+        megaBoard.close()
+        window.destroy()
+
+    b1 = tk.Button(window, text="LHCP", command=lhcp, bg="firebrick2", fg="ghost white", font=("Comic Sans MS", 15))
+
+    b2 = tk.Button(window, text="RHCP", command=rhcp, bg="forest green", fg="gray7", font=("Comic Sans MS", 15))
+
+    b3 = tk.Button(window, text="UPLINK", command=uplink, bg="yellow", fg="gray7", font=("Comic Sans MS", 15))
+
+    b4 = tk.Button(window, text="DOWNLINK", command=downlink, bg="blue", fg="ghost white", font=("Comic Sans MS", 15))
+
+    b5 = tk.Button(window, text="EXIT", command=quit, bg="gold", fg="gray7", font=("Comic Sans MS", 15))
+
+    b1.grid(row=1, column=0, padx=5, pady=10)
+    b2.grid(row=1, column=1, padx=5, pady=10)
+    b3.grid(row=2, column=0, padx=5, pady=10)
+    b4.grid(row=2, column=1, padx=5, pady=10)
+    b5.grid(row=3, column=0, padx=5, pady=10)
+
+    window.mainloop()
+
+time.sleep(2)
+switch_control()
